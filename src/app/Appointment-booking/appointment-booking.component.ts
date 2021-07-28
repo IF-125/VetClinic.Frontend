f56dd325ccb9e5a24c91f8f6197f4af3517358e4
Stanislav Kudriavtsev <equilibrium42@protonmail.com>
2021-07-28 16:43:27 +0300

    Added homepage markup, navigation and footer components.

---
+++ src/app/Appointment-booking/appointment-booking.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'section-booking',
+    templateUrl: './appointment-booking.component.html',
+    styleUrls: ['./appointment-booking.component.scss']
+})
+export class AppointmentBookingComponent {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { FooterComponent } from './Footer/footer.component';
+import { AppointmentBookingComponent } from './Appointment-booking/appointment-booking.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Navigation/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AppointmentBookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
