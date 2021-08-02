a1da7b365e860a7737422b4608c2ef90ab743847
Vasyl Pavelko <kabool21@gmail.com>
2021-08-02 22:26:53 +0300

    Add show pet component

--- src/app/client/show-pet/show-pet.component.ts
+++ src/app/client/show-pet/show-pet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-pet',
@@ -7,9 +8,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowPetComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  PetList:any=[];
+
+  ModalTitle:string;
+  ActivateAddEditPetComp:boolean=false;
+  pet:any;
+
+  PetIdFilter:string="";
+  PetNameFilter:string="";
+  PetListWithoutFilter:any=[];
 
   ngOnInit(): void {
+    this.refreshPetList();
+  }
+
+  addClick(){
+    this.pet={
+      PetId:0,
+      PetName:""
+    }
+    this.ModalTitle="Add Pet";
+    this.ActivateAddEditPetComp=true;
+
+  }
+
+  editClick(item){
+    this.pet=item;
+    this.ModalTitle="Edit Pet";
+    this.ActivateAddEditPetComp=true;
+  }
+
+  deleteClick(item){
+    if(confirm('Are you sure??')){
+      this.service.deletePet(item.PetId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshPetList();
+      })
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditPetComp=false;
+    this.refreshPetList();
+  }
+
+
+  refreshPetList(){
+    this.service.getPetList().subscribe(data=>{
+      this.PetList=data;
+      this.PetListWithoutFilter=data;
+    });
+  }
+
+  FilterFn(){
+    var PetIdFilter = this.PetIdFilter;
+    var PetNameFilter = this.PetNameFilter;
+
+    this.PetList = this.PetListWithoutFilter.filter(function (el){
+        return el.PetId.toString().toLowerCase().includes(
+          PetIdFilter.toString().trim().toLowerCase()
+        )&&
+        el.PetName.toString().toLowerCase().includes(
+          PetNameFilter.toString().trim().toLowerCase()
+        )
+    });
+  }
+
+  sortResult(prop,asc){
+    this.PetList = this.PetListWithoutFilter.sort(function(a,b){
+      if(asc){
+          return (a[prop]>b[prop])?1 : ((a[prop]<b[prop]) ?-1 :0);
+      }else{
+        return (b[prop]>a[prop])?1 : ((b[prop]<a[prop]) ?-1 :0);
+      }
+    })
   }
 
 }
