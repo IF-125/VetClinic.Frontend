09508cd2f65b1ac02c610964871ab352c3732ae3
Vasyl Pavelko <kabool21@gmail.com>
2021-08-02 15:04:09 +0300

    Add home, client page, shared service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { ClientComponent } from './client/client.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PetComponent } from './pet/pet.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'pet',component:PetComponent},
+  {path:'client',component:ClientComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'client/pet', component: HomeComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { AddEditPetComponent } from './pet/add-edit-pet/add-edit-pet.component';
+import { PetComponent } from './pet/pet.component';
+
 import { FooterComponent } from './Footer/footer.component';
 import { AppointmentBookingComponent } from './Appointment-booking/appointment-booking.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,19 +12,38 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Navigation/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedService } from './shared.service';
+
+import{HttpClientModule} from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { ClientComponent } from './client/client.component';
+import { EditProfComponent } from './client/edit-prof/edit-prof.component';
+import { ShowPetComponent } from './client/show-pet/show-pet.component';
+import { HomeComponent } from './home/home.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    AppointmentBookingComponent
+    AppointmentBookingComponent,
+    PetComponent,
+    ShowPetComponent,
+    AddEditPetComponent,
+    ClientComponent,
+    EditProfComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
