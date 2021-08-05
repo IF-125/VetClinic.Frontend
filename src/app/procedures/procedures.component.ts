3212bcb100de5ade6e83a5f468f1263a4674689e
Serge <sanichace@gmail.com>
2021-08-05 23:38:22 +0300

    fixed minor issues

--- src/app/procedures/procedures.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'section-procedures',
-    templateUrl: './procedures.component.html',
-    styleUrls: ['./procedures.component.scss']
-})
-export class ProceduresComponent {
-}
