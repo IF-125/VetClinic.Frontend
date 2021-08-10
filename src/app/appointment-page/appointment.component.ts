1a6a26f9fdcb8a95ab22a429eafcf376cd02549e
Stanislav Kudriavtsev <equilibrium42@protonmail.com>
2021-08-10 19:47:48 +0300

    Minor fixes.

--- src/app/appointment-page/appointment.component.ts
+++ src/app/appointment-page/appointment.component.ts
@@ -1,4 +1,6 @@
+import { PetsService } from 'src/app/services/pets/pets.service';
 import { Component } from "@angular/core";
+import { ProceduresService } from '../services/procedures/procedures.service';
 
 @Component({
     selector: 'section-appointment',
@@ -6,4 +8,31 @@ import { Component } from "@angular/core";
     styleUrls: ['./appointment.component.scss']
 })
 export class AppointmentComponent {
+    constructor(private proceduresService: ProceduresService, private petsService: PetsService) {}
+    
+    showCardForm = false;
+    isDisabled = false;
+    Procedures: any = [];
+    Pets: any = [];
+  
+    ngOnInit(): void {
+      this.getProcedures();
+      this.getPets();
+    }
+  
+    getProcedures() {
+      this.proceduresService.getProcedureList().subscribe((data) => {
+        this.Procedures = data;
+      });
+    }
+
+    getPets() {
+        this.petsService.getPetList().subscribe((data) => {
+          this.Pets = data;
+        });
+      }
+
+    displayCardForm(value){
+        this.showCardForm = value;
+    }
 }
